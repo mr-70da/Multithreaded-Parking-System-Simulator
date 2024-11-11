ae132e285805979d98b07fdc6e350db33a37e5de
khamis <mohamedkhamis20045@gmail.com>
2024-11-11 03:29:03 +0200

    Done

--- Main.java
+++ Main.java
@@ -1,34 +1,6 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ParkingLot parkingLot = new ParkingLot();
-        Log logger = new Log();
-        try
-        {
-            ArrayList<Ticket> tickets = logger.getInput();
-
-            ArrayList<Car> cars = new ArrayList<>();
-
-            for (Ticket ticket : tickets) {
-                Car car = new Car(ticket, parkingLot);
-                car.start();
-                cars.add(car);
-            }
-            for (Car car : cars) {
-                try {
-                    car.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            logger.printLogSummary(tickets);
-        }
-        catch(Exception ex)
-        {
-            System.err.println(ex.getMessage());
-        }
-
+        ParkingSimulator.run();
+        
     }
 }
